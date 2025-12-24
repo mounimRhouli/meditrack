b9dc0760e32e96ce07a22186e277f3d8c006ae37
Karam <karam.lebra55@gmail.com>
2025-12-24 15:38:34 +0100

    Server Exception added

--- lib/core/errors/exceptions.dart
+++ lib/core/errors/exceptions.dart
@@ -15,3 +15,8 @@ class AuthException implements Exception {
   final String message;
   AuthException(this.message);
 }
+
+class ServerException implements Exception {
+  final String message;
+  ServerException(this.message);
+}
